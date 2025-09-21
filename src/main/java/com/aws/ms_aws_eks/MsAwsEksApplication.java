a772917213381e9bed9eca1a2703f23d32589ea8
SajjadHussainGitHub <engr.sajad83@gmail.com>
2025-09-21 12:29:09 -0700

    initial

---
+++ src/main/java/com/aws/ms_aws_eks/MsAwsEksApplication.java
@@ -0,0 +1,13 @@
+package com.aws.ms_aws_eks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAwsEksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAwsEksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/ms_aws_eks/MsAwsEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.ms_aws_eks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAwsEksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
