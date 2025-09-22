aa652cc49aaa33c1d341f6000e834fad464b99a4
SajjadHussainGitHub <engr.sajad83@gmail.com>
2025-09-22 08:44:10 +0400

    changes ...

--- src/main/java/com/aws/ms_aws_eks/controller/GeneralController.java
+++ src/main/java/com/aws/ms_aws_eks/controller/GeneralController.java
@@ -1,9 +1,11 @@
 package com.aws.ms_aws_eks.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class GeneralController {
 
     @GetMapping("/hello")
